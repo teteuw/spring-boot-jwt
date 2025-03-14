b9d283ca7d6b4125e0945b66baed608f11f080c9
Mateus <mateuaraujo01@gmail.com>
2025-03-14 11:49:44 -0300

    aplicando nome do banco de daods para o appuser como user

--- src/main/java/murraco/model/AppUser.java
+++ src/main/java/murraco/model/AppUser.java
@@ -14,7 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.Size;
 
-@Entity
+@Entity(name = "user")
 @Data // Create getters and setters
 @NoArgsConstructor
 public class AppUser {
